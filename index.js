fb3a8e7d5893ee8f3fad432ba4097abaa72ce8c5
Arseniy Sazonov <rimus00@yandex.ru>
2022-07-08 02:14:49 +0300

    Добавить JS

--- index.js
+++ index.js
@@ -1,72 +1,306 @@
-const whiteVin = [
+const whiteVine = [
   {
-    name: 'Оливье Трикон Шабли',
-    country: 'Франция',
-    region: 'Бургундия',
-    manufaktur: 'Оливье Трикон',
-    sort: 'Шардоне',
+    name: "Оливье Трикон Шабли",
+    country: "Франция",
+    region: "Бургундия",
+    manufaktur: "Оливье Трикон",
+    sort: "Шардоне",
   },
   {
     name: "Люньи Л'Аурор Бургонь Шардоне",
-    country: 'Франция',
-    region: 'Бургундия',
-    manufaktur: 'Люньи Ляурор',
-    sort: 'Шардоне',
+    country: "Франция",
+    region: "Бургундия",
+    manufaktur: "Люньи Ляурор",
+    sort: "Шардоне",
   },
   {
-    name: 'Шато де Пареншер Блан',
-    country: 'Франция',
-    region: 'Бордо',
-    manufaktur: 'Шато де Параншер',
-    sort: '55 Совиньен Блан, 30 Сомильон, 15 Мюскадель',
+    name: "Шато де Пареншер Блан",
+    country: "Франция",
+    region: "Бордо",
+    manufaktur: "Шато де Параншер",
+    sort: "55 Совиньен Блан, 30 Сомильон, 15 Мюскадель",
   },
   {
-    name: 'Куатро Райас, 4Р Вердехо',
-    country: 'Испания',
-    region: 'Кастилия и Леон',
-    manufaktur: 'Куатро Райас',
-    sort: 'Вердехо',
+    name: "Куатро Райас, 4Р Вердехо",
+    country: "Испания",
+    region: "Кастилия и Леон",
+    manufaktur: "Куатро Райас",
+    sort: "Вердехо",
   },
   {
-    name: 'Вайнгур Лет, Дуэт Рислинг энд Вельтлинер',
-    country: 'Астрия',
-    region: 'Нижняя Австрия',
-    manufaktur: 'Лет',
-    sort: 'Грюнер Вельтлинер и Рислинг',
+    name: "Вайнгур Лет, Дуэт Рислинг энд Вельтлинер",
+    country: "Австрия",
+    region: "Нижняя Австрия",
+    manufaktur: "Лет",
+    sort: "Грюнер Вельтлинер и Рислинг",
   },
   {
-    name: '',
-    country: '',
-    region: '',
-    manufaktur: '',
-    sort: '',
+    name: "Естрейя Бранко, Виньо Верде",
+    country: "Португалия",
+    region: "Виньо Верде",
+    manufaktur: "Эстрейя",
+    sort: "85 Лоурейру, Трайщадура, Аринту",
   },
   {
-    name: '',
-    country: '',
-    region: '',
-    manufaktur: '',
-    sort: '',
+    name: "Инсайт Совиньон Блан",
+    country: "Новая Зеландия",
+    region: "Мальборо",
+    manufaktur: "Винультура",
+    sort: "Совиньон Блан",
+  },
+];
+
+const redVine = [
+  {
+    name: "Батазиоло Бароло",
+    country: "Италия",
+    region: "Пьемонт",
+    manufaktur: "Бени Батазиоло",
+    sort: "Неббиоло",
   },
   {
-    name: '',
-    country: '',
-    region: '',
-    manufaktur: '',
-    sort: '',
+    name: "Жерардо Чезари Амароне делла Вальполичелла Классико",
+    country: "Италия",
+    region: "Венето",
+    manufaktur: "Жерардо Чизари",
+    sort: "75 Корвина, 20 Рондинелла, 5 Молинара",
   },
   {
-    name: '',
-    country: '',
-    region: '',
-    manufaktur: '',
-    sort: '',
+    name: "Монтечелли, Монтепульчано дАбруцио",
+    country: "Италия",
+    region: "Абруцио",
+    manufaktur: "Монтечелли",
+    sort: "Монтепульчано",
   },
   {
-    name: '',
-    country: '',
-    region: '',
-    manufaktur: '',
-    sort: '',
+    name: "Колли Ирпини Монтесоле Таурази",
+    country: "Италия",
+    region: "Кампания",
+    manufaktur: "Колли Ирпини",
+    sort: "Альянико",
   },
-]
+  {
+    name: "Бальо ди Пьянетто Фраппато",
+    country: "Италия",
+    region: "Сицилия",
+    manufaktur: "Бальо ди Пьянетто",
+    sort: "Фраппато",
+  },
+  {
+    name: "Примитиво Ди Мандурия",
+    country: "Италия",
+    region: "Апулия",
+    manufaktur: "Контри Спуманте",
+    sort: "Примитиво",
+  },
+  {
+    name: "Люньи Ляурор Бургонь Пино Нуар",
+    country: "Франция",
+    region: "Бургундия",
+    manufaktur: "Люньи Ляурор",
+    sort: "Пино Нуар",
+  },
+  {
+    name: "Пьер Гайяр, Сент-Жозеф",
+    country: "Франция",
+    region: "Долина Роны",
+    manufaktur: "Пьер Райяр",
+    sort: "Сира",
+  },
+  {
+    name: "Кастильо де Клавихо, Риоха Резерва",
+    country: "Испания",
+    region: "Риоха",
+    manufaktur: "Кастильо Де Кавихо",
+    sort: "90 Темпранильо, 10 Масуэло",
+  },
+  {
+    name: "Клос Беренгер Клос Де Тафаль",
+    country: "Испания",
+    region: "Приорат",
+    manufaktur: "Клос Берингер",
+    sort: "Гренаш/Гарнача, Каберне Совиньон, Сира",
+  },
+  {
+    name: "Х.Бушон Ресерва Карменере Сира",
+    country: "Чили",
+    region: "Центральная Долина",
+    manufaktur: "Х.Бушон",
+    sort: "Карменере, Сира",
+  },
+  {
+    name: "Гудверахт Вайн Эстейт, Грейт Экспектейшн Шираз",
+    country: "ЮАР",
+    region: "Западный Кейп",
+    manufaktur: "Гудвервах Вейн Эстейт",
+    sort: "Шираз",
+  },
+  {
+    name: "Инсайт Пино Нуар",
+    country: "Новая Зеландия",
+    region: "Мальборо",
+    manufaktur: "Винультура",
+    sort: "Пино Нуар",
+  },
+];
+
+const roseVine = [
+  {
+    name: "Высокий Берег Цвайгельт",
+    country: "Россия",
+    region: "Краснодарский край",
+    manufaktur: "Кубань Вино",
+    sort: "Цвайгельт",
+  },
+];
+
+const igriVine = [
+  {
+    name: "Ла Джойоза Просекко",
+    country: "Италия",
+    region: "Венето",
+    manufaktur: "Вилла Санди",
+    sort: "Глера",
+  },
+  {
+    name: "Мартини Просекко",
+    country: "Италия",
+    region: "Венето",
+    manufaktur: "Бакарди Мартини",
+    sort: "Глера",
+  },
+  {
+    name: "Мартини Асти",
+    country: "Италия",
+    region: "Пьемонт",
+    manufaktur: "Бакарди Мартини",
+    sort: "Белый Мускат",
+  },
+  {
+    name: "Инкерман Пино Блан Зеро Дозаж Эекстра Брют",
+    country: "Россия",
+    region: "Крым, Севастополь",
+    manufaktur: "Инкерман",
+    sort: "Пино Блан",
+  },
+  {
+    name: "Ганча Розе",
+    country: "Италия",
+    region: "Пьемонт",
+    manufaktur: "Ганча",
+    sort: "Пино Неро, Пино Бьянко",
+  },
+  {
+    name: "Ганча Брют",
+    country: "Италия",
+    region: "Пьемонт",
+    manufaktur: "Ганча",
+    sort: "Шардоне",
+  },
+  {
+    name: "Драпье Брют Натюр",
+    country: "Франция",
+    region: "Шампань",
+    manufaktur: "Драпье",
+    sort: "Пино Нуар",
+  },
+];
+
+const menuLink = document.querySelector(".menus");
+const next = document.querySelector(".next");
+const back = document.querySelector(".back");
+
+const menuVine = document.querySelector(".menu-vine");
+const red = menuVine.querySelector(".red");
+const white = menuVine.querySelector(".white");
+const igri = menuVine.querySelector(".igri");
+
+const content = document.querySelector(".content");
+const contName = content.querySelector(".name");
+const contCountry = content.querySelector(".country");
+const contRegion = content.querySelector(".region");
+const contManuf = content.querySelector(".manufaktur");
+const contSort = content.querySelector(".sort");
+
+function openMenu() {
+  menuVine.classList.toggle("disabled");
+  if (!content.classList.contains("disabled")) {
+    content.classList.add("disabled");
+    next.classList.add("disabled");
+    back.classList.add("disabled");
+  }
+}
+
+function openContent() {
+  menuVine.classList.add("disabled");
+  content.classList.remove("disabled");
+  next.classList.remove("disabled");
+  back.classList.remove("disabled");
+}
+
+function changeMain(date) {
+  let q = 0;
+  contName.textContent = date[q].name;
+  contCountry.textContent = date[q].country;
+  contRegion.textContent = date[q].region;
+  contManuf.textContent = date[q].manufaktur;
+  contSort.textContent = date[q].sort;
+  next.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    console.log('HHH')
+    q++;
+    if (q > 0){
+      back.classList.remove("disabled");
+    }
+    if (q === date.length - 1) {
+      next.classList.add("disabled");
+    }
+    contName.textContent = date[q].name;
+    contCountry.textContent = date[q].country;
+    contRegion.textContent = date[q].region;
+    contManuf.textContent = date[q].manufaktur;
+    contSort.textContent = date[q].sort;
+  });
+  back.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    q--;
+    contName.textContent = date[q].name;
+    contCountry.textContent = date[q].country;
+    contRegion.textContent = date[q].region;
+    contManuf.textContent = date[q].manufaktur;
+    contSort.textContent = date[q].sort;
+    if (q === 0){
+      back.classList.add("disabled");
+      next.classList.remove('disabled')
+    }
+  });
+}
+
+/*function changeMain(date) {
+  let k = date;
+  let q = 0;
+  contName.textContent = k[q].name;
+  contCountry.textContent = k[q].country;
+  contRegion.textContent = k[q].region;
+  contManuf.textContent = k[q].manufaktur;
+  contSort.textContent = k[q].sort;
+}*/
+
+menuLink.addEventListener("click", openMenu);
+red.addEventListener("click", (evt) => {
+  evt.preventDefault()
+  openContent();
+  changeMain(redVine);
+});
+
+white.addEventListener("click", (evt) => {
+  evt.preventDefault()
+  openContent();
+  changeMain(whiteVine);
+});
+
+igri.addEventListener("click", (evt) => {
+  evt.preventDefault()
+  openContent();
+  changeMain(igriVine);
+});
